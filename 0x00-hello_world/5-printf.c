d43fdb8ca1f8073f810098cfe306eddd472cf7e9
Bongane2023 <bonganetonic@gmail.com>
2023-06-19 05:01:04 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
+ * main - A program that print a line using the ptintf f
+unction
  * Return: Always 0 (success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a pieceof art,\n")
+return (0);
 }
